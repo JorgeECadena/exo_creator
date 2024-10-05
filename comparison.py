33276af772b5e9f48c03c2cc69c8b90d66646a9c
BarbieMunoz <a01659348@tec.mx>
2024-10-05 15:45:20 -0600

    compares the best fit

--- comparison.py
+++ comparison.py
@@ -1,1 +1,26 @@
+import pandas as pd
+from scipy.spatial.distance import euclidean
 
+df = pd.read_csv('data.csv')
+
+radius = 18.647
+mass = 2543
+temperature = 1800
+values = [radius, mass, temperature]
+
+def similarity(df, values):
+    # if column not num -> fill with 0
+    df[['pl_rade', 'pl_bmasse', 'pl_eqt']] = df[['pl_rade', 'pl_bmasse', 'pl_eqt']].apply(pd.to_numeric, errors='coerce').fillna(0)
+
+    # calculate with each roww
+    df['resultDistance'] = df[['pl_rade', 'pl_bmasse', 'pl_eqt']].apply(lambda row: euclidean(row, values), axis=1)
+
+    # min distance
+    mostCompatible = df['resultDistance'].idxmin()
+    exoplanet = df.iloc[mostCompatible]['pl_name']
+
+    return exoplanet
+
+compatible_planet = similarity(df, values)
+
+print(compatible_planet)
