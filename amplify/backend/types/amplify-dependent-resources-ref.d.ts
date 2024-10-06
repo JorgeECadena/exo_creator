5a22c40ac95ea91788a68e3ef91fd4b943d9057d
Jorge Cadena <jorge-c@Jorges-MacBook-Pro.local>
2024-10-05 21:49:08 -0600

    Added client

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nodeApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "nodeFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { get } from 'aws-amplify/api';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API = 'nodeApi';
+const path = '/node';
+
+const App = () => {
+
+  async function getMessage() {
+    try {
+      const restOp = get({
+        apiName: API,
+        path: path,
+      });
+
+      const { body } = await restOp.response;
+      const message = await body.json();
+
+      console.log("GET operation successful: ", message);
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>YA FUNCIONA POR FAVOR</h1>
+      <button onClick={ () => getMessage()}>Test</button>
+    </div>
   )
 }
 
