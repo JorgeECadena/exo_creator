c4d470bd2234643392f6ff58afbb7bc0f4434b16
bote24 <a01659281@itesm.mx>
2024-10-05 19:06:15 -0600

    cambios a main
    Inputs en ingles, creacion de un hipervinculo a la pagina de la nasa con ese planeta

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import pandas as pd
 from comparison import similarity
 from comprobar_vida import check_life
 from distancias import Distancias
+from conseguir_url import link
 
 df = pd.read_csv('data.csv')
-planeta = input('Ingrese el nombre del planeta: ')
-distancia = float(input('Ingrese la distancia del planeta a su estrella (en AU): '))
-masa_sol = float(input('Ingrese la masa de la estrella (en Soles): '))
-temperatura = float(input('Ingrese la temperatura de equilibrio del planeta (en °C): '))
-masa_planeta = float(input('Ingrese la masa del planeta (en Tierras): '))
-radio_planeta = float(input('Ingrese el radio del planeta (en Tierras): '))
+planeta = input('Enter the name of the planet: ')
+
+def get_float_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Please enter a valid number.")
+
+distancia = get_float_input('Enter the distance of the planet to its star (in AU): ')
+masa_sol = get_float_input('Enter the mass of the star (in Suns): ')
+temperatura = get_float_input('Enter the equilibrium temperature of the planet (in °C): ')
+masa_planeta = get_float_input('Enter the mass of the planet (in Earths): ')
+radio_planeta = get_float_input('Enter the radius of the planet (in Earths): ')
 values = [radio_planeta, masa_planeta, temperatura, masa_sol, distancia]
 
 planeta_parecido = similarity(df, values)
-print('El planeta más parecido es: ', planeta_parecido['pl_name'])
-print('¿Es habitable? ', check_life(distancia, masa_sol, temperatura))
+print('The most similar planet is: ', planeta_parecido['pl_name'])
+print('Is it habitable? ', check_life(distancia, masa_sol, temperatura))
 
 distanciaNave = Distancias(distancia)
-print('El tiempo de llegar con una nave de Star Trek al planeta son: ', distanciaNave.starTrek(), 'años')
-print('El tiempo de llegar con una nave de Star Wars al planeta son: ', distanciaNave.starWars(), 'años')
-print('El tiempo de llegar con una nave de Space Odyssey al planeta son: ', distanciaNave.SpaceOdyssey(), 'años')
-print('El tiempo de llegar con una nave de Halo al planeta son: ', distanciaNave.halo(), 'años')
+print('The travel time with a Star Trek ship to the planet is: ', distanciaNave.starTrek(), 'years')
+print('The travel time with a Star Wars ship to the planet is: ', distanciaNave.starWars(), 'years')
+print('The travel time with a Space Odyssey ship to the planet is: ', distanciaNave.SpaceOdyssey(), 'years')
+print('The travel time with a Halo ship to the planet is: ', distanciaNave.halo(), 'years')
+
+print(f'For more information about the planet: "{planeta_parecido["pl_name"]}", visit: {link(planeta_parecido["pl_name"])}')
